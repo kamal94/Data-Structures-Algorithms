22490fd9f862b6423e927b46f62649388089540c
Kamal Kamalaldin <ask@kamalaldin.com>
2015-11-16 23:27:34 -0500

    Fixed a bug in GraphNode in connecting nodes and testing for the existance of edges

---
+++ DataStructures/Graph/GraphNodeTest.java
@@ -0,0 +1,36 @@
+package DataStructures.Graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kamal on 11/11/15.
+ */
+public class GraphNodeTest {
+
+    @Test
+    public void testConnectNode() throws Exception {
+
+    }
+
+    @Test
+    public void testConnectedTo() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEdges() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConnectedNodes() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+}
